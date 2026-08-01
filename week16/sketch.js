8270fcb0457e8e23983b6685a91398dcd8dbcfe4
Chiang Chenwei <skipclass@gmail.com>
2026-08-01 12:37:54 +0800

    template 精簡為空白環境：移除 16 週骨架，作業資料夾由學生自建（四件套範本改內嵌 README）

--- week16/sketch.js
+++
@@ -1,9 +0,0 @@
-// 在這裡寫（或貼上你工作流產出的）p5.js 程式碼
-function setup() {
-  createCanvas(600, 600)
-}
-
-function draw() {
-  background(253, 252, 249)
-  circle(width / 2, height / 2, 100 + 20 * sin(frameCount * 0.05))
-}
